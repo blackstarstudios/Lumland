14d45fbc4a230c93db5b504db19dda179123d4ca
malgrieve <161348075+malgrieve@users.noreply.github.com>
2025-02-17 00:04:05 -0500

    Update lumland.py

--- lumland.py
+++ lumland.py
@@ -4,7 +4,7 @@
 # ====================================================================== LIBRARIES ===========================================================================
 
 # Official Libraries
-
+import os
 
 # Custom Libraries
 
@@ -13,6 +13,7 @@
 
 # Major Game States
 run = True
+menu = True
 play = False
 pause = False
 options = False
@@ -30,7 +31,31 @@ enchant = False
 
 # ======================================================================== DESIGN ============================================================================
 
+# Clear terminal
+def clr():
+    os.system('cls' if os.name == 'nt' else 'clear') # cls - Windows; clear - Unix
 
+# Write borderline
+def brdl():
+    print("!=========================!")
+    #print(" ")
+
+# Write borderline
+#def brdb():
+#    print(" ")
+#    print("!=====================================!")
+
+# Write underline
+def undl():
+    print("________________")
+
+# Write spaceline
+def spcl():
+    print(" ")
+
+# Write sectionline
+def secl():
+    print("+-----------------+")
 
 # ======================================================================== ITEMS =============================================================================
 
@@ -56,7 +81,7 @@ class Player:
         self.AP = AP
         self.MP = MP
         self.SP = SP
-        self.lums = 0
+        self.lums = lums
 
 # ====================================================================== FUNCTIONS ===========================================================================
 
@@ -89,18 +114,62 @@ def alchemy():
 
 # ======================================================================== LOGIC =============================================================================
 
+def rules():
+    print("rules")
+
+def options():
+    print("options")
+
+# Exit game to main menu
+def exit():
+    pass
+
+# Save game file
 def save():
     pass
 
+# Load game file
 def load():
     pass
+
+# Pause game text
+def cont():
+    input("> ")
+
+# In-game choice
+def opt():
+    print(" ")
+    o = int(input(" > "))
+    return o
 
 # ==================================================================== MAIN GAME LOOP ========================================================================
 
 def lumland():
     while run:
-        while play:
-            pass
+        while menu:
+            clr()
+            brdl()
+            print("   LUMLAND")
+            spcl()
+            print("1. NEW GAME")
+            print("2. LOAD GAME")
+            print("3. OPTIONS")
+            print("4. RULES")
+            print("5. QUIT")
+            brdl()
+
+            o = opt()
+
+            if o == 1:
+                pass
+            elif o == 2:
+                pass
+            elif o == 3:
+                pass
+            elif o == 4:
+                rules()
+            elif o == 5:
+                quit()
 
 # Game Function
 lumland()
