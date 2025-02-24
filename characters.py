b367725c667eb7b5a09c61fef44ebdf46ce5b3ef
malgrieve <161348075+malgrieve@users.noreply.github.com>
2025-02-24 01:03:26 -0500

    Create characters.py

---
+++ characters.py
@@ -0,0 +1,26 @@
+class Characters:
+    def __init__(self, name, race, title, job, hp, hpmax, ap, apmax, mp, mpmax, sp, spmax, lvl, exp, expmax, rep, lums, primary, secondary, magics, arts, skills, x, y):
+        self.name
+        self.race
+        self.title
+        self.job
+        self.hp
+        self.hpmax
+        self.ap
+        self.apmax
+        self.mp
+        self.mpmax
+        self.sp
+        self.spmax
+        self.lvl
+        self.exp
+        self.expmax
+        self.rep
+        self.lums
+        self.primary
+        self.secondary
+        self.magics
+        self.arts
+        self.skills
+        self.x
+        self.y
