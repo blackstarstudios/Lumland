dcaa917c122623c5b3d14c02f1a3b3fad044ca1e
malgrieve <161348075+malgrieve@users.noreply.github.com>
2025-02-25 06:36:17 -0500

    Update characters.py

--- NPCs/Characters/characters.py
+++ NPCs/Characters/characters.py
@@ -1,26 +1,27 @@
 class Characters:
-    def __init__(self, name, race, title, job, hp, hpmax, ap, apmax, mp, mpmax, sp, spmax, lvl, exp, expmax, rep, lums, primary, secondary, magics, arts, skills, x, y):
-        self.name
-        self.race
-        self.title
-        self.job
-        self.hp
-        self.hpmax
-        self.ap
-        self.apmax
-        self.mp
-        self.mpmax
-        self.sp
-        self.spmax
-        self.lvl
-        self.exp
-        self.expmax
-        self.rep
-        self.lums
-        self.primary
-        self.secondary
-        self.magics
-        self.arts
-        self.skills
-        self.x
-        self.y
+    def __init__(self, name, race, title, class, job, hp, hpmax, ap, apmax, mp, mpmax, sp, spmax, lvl, exp, expmax, rep, lums, primary, secondary, magics, arts, skills, x, y):
+        self.name = name
+        self.race = race
+        self.title = title
+        self.class = class
+        self.job = job
+        self.hp = hp
+        self.hpmax = hpmax
+        self.ap = ap
+        self.apmax = apmax
+        self.mp = mp
+        self.mpmax = mpmax
+        self.sp = sp
+        self.spmax = spmax
+        self.lvl = lvl
+        self.exp = exp
+        self.expmax = expmax
+        self.rep = rep
+        self.lums = lums
+        self.primary = primary
+        self.secondary = secondary
+        self.magics = magics
+        self.arts = arts
+        self.skills = skills
+        self.x = x
+        self.y = y
